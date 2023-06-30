aa2b0facb42a80722a1897d2e792631b2e242d58
Jason Tavares <jwtavare@gmail.com>
2023-06-30 14:49:11 -0400

    Last

--- main.go
+++ main.go
@@ -6,4 +6,10 @@ func main() {
 	fmt.Println("Hello!! 🍌 👶 💢")
 	const name, age = "Kim", 22
 	fmt.Printf("%s is %d years old.\n", name, age)
+	fmt.Println(`
+	
+	There once was a lady named muffet, who sat on hter tuffet, eating her curds
+	and weight.  Along came a spider and sat down beside her and...yoou know the rest...
+	
+	`)
 }
