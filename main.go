4286636613ed2e9d4d933762749d153cc1013323
Jason Tavares <jwtavare@gmail.com>
2023-08-10 14:44:01 -0400

    commit

--- main.go
+++ main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"math/rand"
+	"time"
 )
 
 func main() {
 	fmt.Println(rand.Intn(5))
+	fmt.Println("Time current time is", time.Now().AddDate(-1,2,3))
+	fmt.Println("Let's find some squares ",math.Sqrt(255))
 	fmt.Println("Hello!! 🍌 👶 💢")
+	fmt.Println(math.Pi)
 	const name, age = "JT and Iqy 4501100", 22
 	fmt.Printf("%s is %d years old.\n", name, age)
 	fmt.Println(`
